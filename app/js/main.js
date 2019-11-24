c38a8465bf4f4e83960227ef95aac8b596fccb26
St4y-23 <ghost19011@gmail.com>
2019-11-24 19:35:16 +0400

    added header, main, agency, features

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
+  $('.comforts__inner-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev arrow__prev"><img src="images/svg/slide-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next arrow__next"><img src="images/svg/slide-next.svg" alt=""></button>',
+  });
   
 
 });
